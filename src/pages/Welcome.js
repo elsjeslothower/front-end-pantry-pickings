9caa7ed7c821f6b93376e9e1e07fbbe1d65c549b
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-02 13:39:57 -0800

    adding image to homepage

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import "./welcome.css"
 
+const burger = require('../../public/images/');
+
 const Welcome = () => {
   return (
-    <div>
+    <div id="welcome">
       <h1>Welcome to Pantry Pickings!</h1>
+      <img id="burger" src={burger} alt="burger"></img>
       <Link to="/login"><button>Click here to login</button></Link>
       <p>Don't have an account?</p>
       <Link to="/signup">Sign up here</Link>
