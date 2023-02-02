a55ee9c309c30fbdc0e8e00540ea21788bdf58bb
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-02 14:04:20 -0800

    Recipe component barebones

---
+++ src/components/Recipe.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Recipe.css";
+
+const Recipe = (props) => {
+  return (
+    <div className="individual-recipe">
+      <h2>{props.recipe_title}</h2>
+      <a target="_blank" href={props.source_url}>
+        <img src={props.recipe_img} alt={`completed dish: ${props.recipe_title}`}/>
+      </a>
+      <ul>
+        <li>recipe #{props.api_id}</li>
+        <li>summary: {props.summary}</li>
+      </ul>
+    </div>
+  );
+};
+
+Recipe.propTypes = {
+  recipe_id: PropTypes.number.isRequired,
+  api_id: PropTypes.number.isRequired,
+  recipe_title: PropTypes.string,
+  summary: PropTypes.string,
+  source_url: PropTypes.string,
+  recipe_img: PropTypes.string,
+};
+
+// Need to add used/missing ingredient count?
+
+export default Recipe;
