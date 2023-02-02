399aaa85d4ddda1b367ec7ca14df33b0f514aaae
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-02 13:52:05 -0800

    pantryItem presentational component

---
+++ src/components/PantryItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./PantryItem.css";
+
+const PantryItem = (props) => {
+  return (
+    <div className="individual-item">
+      <h1>{props.pantry_item_title}</h1>
+      <ul>
+        <li>{props.category}</li>
+        <li>{props.exp_date}</li>
+      </ul>
+    </div>
+  );
+};
+
+PantryItem.propTypes = {
+  pantry_item_id: PropTypes.number.isRequired,
+  pantry_item_title: PropTypes.string.isRequired,
+  category: PropTypes.string,
+  exp_date: PropTypes.string,
+};
+
+export default PantryItem;
