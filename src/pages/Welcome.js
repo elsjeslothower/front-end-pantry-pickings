4bfc2bc85705dd2f0f288c8c9d98b34d3d8f549f
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-06 11:48:50 -0800

    welcome CSS

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -7,8 +7,8 @@ const Welcome = () => {
   return (
     <div className="container" id="welcome">
       <h1>Welcome to Pantry Pickings!</h1>
-      <img id="burger" src={burger} alt="burger"></img>
-      <div className="p-2"><Link to="/login"><button>Click here to login</button></Link></div>
+      <img id="burger" className="m-3" src={burger} alt="burger"></img>
+      <div className="p-2"><Link to="/login"><button type="submit" className="btn btn-warning">Click here to login</button></Link></div>
       <p>
         Don't have an account? <Link to="/register">Sign up here</Link>
       </p>
