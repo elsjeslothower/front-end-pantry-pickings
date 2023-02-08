4eda1b6012da132527daa987af90b3714c48b25e
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-08 14:52:52 -0800

    mock data to test frontend with

---
+++ src/mockData/mockPantry.js
@@ -0,0 +1,52 @@
+const mockPantry = [
+  {
+    pantry_item_id: 1,
+    pantry_item_title: "Eggs",
+    category: "Poultry",
+    exp_date: "12/12/2024",
+  },
+  {
+    pantry_item_id: 2,
+    pantry_item_title: "Bread",
+    category: "Grains",
+    exp_date: "12/11/2023",
+  },
+  {
+    pantry_item_id: 1,
+    pantry_item_title: "Eggs",
+    category: "Poultry",
+    exp_date: "12/12/2024",
+  },
+  {
+    pantry_item_id: 2,
+    pantry_item_title: "Bread",
+    category: "Grains",
+    exp_date: "12/11/2023",
+  },
+  {
+    pantry_item_id: 1,
+    pantry_item_title: "Eggs",
+    category: "Poultry",
+    exp_date: "12/12/2024",
+  },
+  {
+    pantry_item_id: 2,
+    pantry_item_title: "Bread",
+    category: "Grains",
+    exp_date: "12/11/2023",
+  },
+  {
+    pantry_item_id: 1,
+    pantry_item_title: "Eggs",
+    category: "Poultry",
+    exp_date: "12/12/2024",
+  },
+  {
+    pantry_item_id: 2,
+    pantry_item_title: "Bread",
+    category: "Grains",
+    exp_date: "12/11/2023",
+  },
+]
+
+export default mockPantry;
---
+++ src/mockData/mockRecipes.js
@@ -0,0 +1,20 @@
+const mockRecipes = [
+  {
+    recipe_id: 3,
+    api_id: 7980,
+    recipe_title: "Greek Chickpeas with Spinach",
+    summary: "A delicious meal that takes almost no time to whip up. Trader Joe's is the MVP here",
+    source_url: "https://www.traderjoes.com/home",
+    recipe_img: "insert_pic_here.jpg",
+  },
+  {
+    recipe_id: 4,
+    api_id: 2534,
+    recipe_title: "Tuna Melt",
+    summary: "A healthy sandwich with no top slice of bread",
+    source_url: "https://chickenofthesea.com/",
+    recipe_img: "insert_pic_here.jpg",
+  },
+]
+
+export default mockRecipes;
