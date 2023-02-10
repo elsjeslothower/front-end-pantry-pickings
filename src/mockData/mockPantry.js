6ce1d834decf524bd42931d3aa1218e9e49d776e
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-10 14:08:49 -0800

    refined mock data

--- src/mockData/mockPantry.js
+++ src/mockData/mockPantry.js
@@ -13,14 +13,14 @@ const mockPantry = [
   },
   {
     pantry_item_id: 3,
-    pantry_item_title: "Eggs",
-    category: "Poultry",
+    pantry_item_title: "Milk",
+    category: "Dairy",
     exp_date: "12/12/2024",
   },
   {
     pantry_item_id: 4,
-    pantry_item_title: "Bread",
-    category: "Grains",
+    pantry_item_title: "Butter",
+    category: "Dairy",
     exp_date: "12/11/2023",
   },
   {
@@ -31,20 +31,20 @@ const mockPantry = [
   },
   {
     pantry_item_id: 6,
-    pantry_item_title: "Bread",
-    category: "Grains",
+    pantry_item_title: "Ground beef",
+    category: "Meat",
     exp_date: "12/11/2023",
   },
   {
     pantry_item_id: 7,
-    pantry_item_title: "Eggs",
-    category: "Poultry",
+    pantry_item_title: "Chicken stock",
+    category: "Cooking items",
     exp_date: "12/12/2024",
   },
   {
     pantry_item_id: 8,
-    pantry_item_title: "Bread",
-    category: "Grains",
+    pantry_item_title: "Apples",
+    category: "Produce",
     exp_date: "12/11/2023",
   },
 ]
