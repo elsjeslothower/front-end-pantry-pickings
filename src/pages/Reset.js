a2ac7fe9dfedfaef066a8d1b3dc56d0c8d8ad49e
Elsje Slothower <elsje.slothower@gmail.com>
2023-02-13 17:01:51 -0800

    create Reset.js

---
+++ src/pages/Reset.js
@@ -0,0 +1,12 @@
+import PasswordResetForm from "../components/forms/PasswordResetForm";
+
+const Reset = () => {
+  return (
+    <div className="container">
+      <h1 className="display-1 p-4">Reset password</h1>
+      <PasswordResetForm />
+    </div>
+  );
+};
+
+export default Reset;
